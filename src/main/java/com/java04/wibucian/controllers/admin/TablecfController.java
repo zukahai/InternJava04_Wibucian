c461562cf064e4049d22a54df9b66cce493edff7
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-08-05 20:28:15 +0700

    Xong them ban

--- src/main/java/com/java04/wibucian/controllers/admin/TablecfController.java
+++ src/main/java/com/java04/wibucian/controllers/admin/TablecfController.java
@@ -1,13 +1,11 @@
 package com.java04.wibucian.controllers.admin;
 
 import com.java04.wibucian.dtos.TablecfDTO;
-import com.java04.wibucian.models.Tablecf;
-import com.java04.wibucian.models.TypeTable;
 import com.java04.wibucian.services.TablecfService;
+import com.java04.wibucian.services.TypeTableService;
 import com.java04.wibucian.vos.TablecfQueryVO;
 import com.java04.wibucian.vos.TablecfUpdateVO;
 import com.java04.wibucian.vos.TablecfVO;
-import com.java04.wibucian.vos.TypeTableVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
@@ -17,11 +15,8 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
 @Validated
 @Controller
@@ -30,15 +25,21 @@ public class TablecfController {
 
     @Autowired
     private TablecfService tablecfService;
+    private TypeTableService typeTableService;
+
+    public TablecfController(TypeTableService typeTableService) {
+        this.typeTableService = typeTableService;
+    }
+
     @GetMapping("/")
     public String Home(ModelMap modelMap)throws Exception {
-
         modelMap.addAttribute("tables", tablecfService.findAll());
         return "admin/table/index";
     }
 
     @GetMapping("/create")
     public String createProductPage(ModelMap modelMap) throws Exception {
+        modelMap.addAttribute("typeTables", typeTableService.findAll());
         return "admin/table/create";
     }
 
