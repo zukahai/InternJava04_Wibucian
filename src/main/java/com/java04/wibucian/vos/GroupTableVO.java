3418d14d93bb1b950bd9064af304bbea92282dc8
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-08-06 00:46:34 +0700

    Xong chuc nang them nhom ban

--- src/main/java/com/java04/wibucian/vos/GroupTableVO.java
+++ src/main/java/com/java04/wibucian/vos/GroupTableVO.java
@@ -11,12 +11,6 @@ import java.util.Date;
 public class GroupTableVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @NotNull(message = "id can not null")
-    private Integer id;
-
-    @NotNull(message = "idGroupTable can not null")
-    private String idGroupTable;
-
     private String groupName;
 
     private Date foundedTime;
