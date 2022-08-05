e797cb6e223f4e25a841b3c498be7e5d1f6432ba
Your Name <you@example.com>
2022-08-05 16:07:24 +0700

    local

--- src/main/java/com/java04/wibucian/repositories/AccountRepository.java
+++ src/main/java/com/java04/wibucian/repositories/AccountRepository.java
@@ -3,10 +3,11 @@ package com.java04.wibucian.repositories;
 import com.java04.wibucian.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, String>, JpaSpecificationExecutor<Account> {
-
-    Optional<Account> findAccountById(String username);
+    Optional<Account> findAccountById(String id);
 }
--- src/main/java/com/java04/wibucian/security/SecurityConfig.java
+++ src/main/java/com/java04/wibucian/security/SecurityConfig.java
@@ -13,71 +13,105 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private MyUserDetailsService myUserDetailsService;
+
+    public SecurityConfig(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/").hasRole("ADMIN")
+                .antMatchers("/staff/").hasRole("STAFF")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
     /**
      * Register CustomAccessDeniedHandler as a Java Bean
      *
      * @return CustomAccessDeniedHandler
      */
-    @Bean
-    public CustomAccessDeniedHandler accessDeniedHandler() {
-        return new CustomAccessDeniedHandler();
-    }
+//    @Bean
+//    public CustomAccessDeniedHandler accessDeniedHandler() {
+//        return new CustomAccessDeniedHandler();
+//    }
+//
+//    /**
+//     * Register a temporary InMemoryDatabase for authentication
+//     * in real project we would use DaoAuthenticationProvider
+//     * @return
+//     */
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        PasswordEncoder encoder =
+//                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        UserDetails staffUser = User.builder()
+//                                    .username("staff")
+//                                    .password(encoder.encode("password"))
+//                                    .roles("STAFF")
+//                                    .build();
+//        UserDetails adminUser = User.builder()
+//                                    .username("admin")
+//                                    .password(encoder.encode("password"))
+//                                    .roles("ADMIN")
+//                                    .build();
+//        return new InMemoryUserDetailsManager(staffUser, adminUser);
+//    }
+//
+//    /**
+//     * Specify and register security chain
+//     * @param http HttpSecurity builder
+//     * @return SecurityFilterChain
+//     * @throws Exception Exception
+//     */
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws
+//            Exception {
+//        // disable csrf because we have no csrf token in form
+//        http.csrf()
+//            .disable();
+//
+//        http.authorizeRequests()
+//            .antMatchers("/admin")
+//            .hasRole("ADMIN")
+//            .and()
+//            .authorizeRequests()
+//            .antMatchers("/staff")
+//            .hasRole("STAFF")
+//            .and()
+//            .exceptionHandling()
+//            .accessDeniedHandler(accessDeniedHandler());
+//
+//        http.formLogin();
+//
+//        return http.build();
+//    }
 
-    /**
-     * Register a temporary InMemoryDatabase for authentication
-     * in real project we would use DaoAuthenticationProvider
-     * @return
-     */
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        PasswordEncoder encoder =
-                PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        UserDetails staffUser = User.builder()
-                                    .username("staff")
-                                    .password(encoder.encode("password"))
-                                    .roles("STAFF")
-                                    .build();
-        UserDetails adminUser = User.builder()
-                                    .username("admin")
-                                    .password(encoder.encode("password"))
-                                    .roles("ADMIN")
-                                    .build();
-        return new InMemoryUserDetailsManager(staffUser, adminUser);
-    }
 
-    /**
-     * Specify and register security chain
-     * @param http HttpSecurity builder
-     * @return SecurityFilterChain
-     * @throws Exception Exception
-     */
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws
-            Exception {
-        // disable csrf because we have no csrf token in form
-        http.csrf()
-            .disable();
 
-        http.authorizeRequests()
-            .antMatchers("/admin")
-            .hasRole("ADMIN")
-            .and()
-            .authorizeRequests()
-            .antMatchers("/staff")
-            .hasRole("STAFF")
-            .and()
-            .exceptionHandling()
-            .accessDeniedHandler(accessDeniedHandler());
-
-        http.formLogin();
-
-        return http.build();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
     }
 }
