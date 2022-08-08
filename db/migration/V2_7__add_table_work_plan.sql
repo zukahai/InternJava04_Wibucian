8fd4827b5c50446051a499e2f5861ce3130c09f7
long <longvavet1325@gmail.com>
2022-08-08 22:11:09 +0700

    [FIX] db migration file

--- db/migration/V2_7__add_table_work_plan.sql
+++ db/migration/V2_7__add_table_work_plan.sql
@@ -7,7 +7,7 @@ drop table if exists WorkPlan
 create table WorkPlan
 (
     id        int IDENTITY(1,1),
-    idWorkPlanDetail AS [dbo].getUniqueIdWithPrefix1(id, 'WorkPlan') PERSISTED primary key,
+    idWorkPlan AS [dbo].getUniqueIdWithPrefix1(id, 'WorkPlan') PERSISTED primary key,
     shiftDate date,
     shiftCode int,
     work      bit
