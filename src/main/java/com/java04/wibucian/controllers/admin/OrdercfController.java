90616e30e45feefceb5e51538731b1168bd7bb91
Nam077 <nvnam.18it5@vku.udn.vn>
2022-08-14 01:43:03 +0700

    fix order and recode order fix ingredient error

--- src/main/java/com/java04/wibucian/controllers/admin/OrdercfController.java
+++ src/main/java/com/java04/wibucian/controllers/admin/OrdercfController.java
@@ -7,6 +7,7 @@ import com.java04.wibucian.models.ProductNoMapPing;
 import com.java04.wibucian.services.GroupTableService;
 import com.java04.wibucian.services.OrdercfService;
 import com.java04.wibucian.services.ProductService;
+import com.java04.wibucian.vos.OrdercfUpdateVO;
 import com.java04.wibucian.vos.OrdercfVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -39,6 +40,17 @@ public class OrdercfController {
         modelMap.addAttribute("products", productService.listAll());
         return "admin/order/index";
     }
+    @PostMapping( produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<Object> save(@RequestBody OrdercfVO ordercfVO) {
+        return ResponseEntity.ok().body(ordercfService.save(ordercfVO));
+    }
+
+    @PutMapping(value = "/{id}", produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public ResponseEntity<Object> update(@PathVariable("id") String id, @RequestBody OrdercfUpdateVO vo) {
+        return ResponseEntity.ok().body(ordercfService.update(id, vo));
+    }
 
     @RequestMapping(value = {"/store","/store/"}, method = RequestMethod.POST,
             produces = "application/json; charset=utf-8")
@@ -62,27 +74,12 @@ public class OrdercfController {
             return ResponseEntity.ok().body(map);
         }
     }
-    @RequestMapping(value = {"/store-final","/store-final/"}, method = RequestMethod.POST,
+    @RequestMapping(value = {"/store-final/{id}"}, method = RequestMethod.PUT,
             produces = "application/json; charset=utf-8")
-    @ResponseBody
-    public ResponseEntity<Object> store_final(@RequestBody List<OrdercfVO> ordercfList) {
-        try {
-            for(OrdercfVO ordercfVO: ordercfList){
-                ordercfService.save(ordercfVO);
-            }
-            HashMap<String, Object> map = new HashMap<>();
-            map.put("check", true);
-            map.put("value", "test");
-            return ResponseEntity.ok().body(map);
-        } catch (Exception e) {
-            e.printStackTrace();
-            HashMap<String, Object> map = new HashMap<>();
-            map.put("check", false);
-            map.put("value", "test");
-            return ResponseEntity.ok().body(map);
-        }
+    public ResponseEntity<Object> storeFinal(@PathVariable("id") String id, @RequestBody OrdercfUpdateVO vo) {
+        return ResponseEntity.ok().body(ordercfService.update_final(id, vo));
     }
-    //find Ordercf by findByGroupTableId
+
     @RequestMapping(value = "/find-by-group-table/{id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     @ResponseBody
     public ResponseEntity<Object> findByGroupTableId(@PathVariable("id") String id) {
