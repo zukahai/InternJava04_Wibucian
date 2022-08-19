69b204b18d751fb53ded10384f154afe8f385d94
Nam077 <nvnam.18it5@vku.udn.vn>
2022-08-19 08:06:35 +0700

    fix db

--- db/migration/V3_5__alter_tabel_invoice_customer_allow_nullt.sql
+++ db/migration/V3_5__alter_tabel_invoice_customer_allow_nullt.sql
@@ -1,7 +1,3 @@
 use wibucian
 go
-alter table invoice add status int
-//set customer name allow null to true
-use wibucian
-go
-alter table Invoice customerName nvarchar (50) null,
+alter table Invoice alter column customerName  nvarchar (50) null
